c3e228823345c3b1f15ac3210dae7ecf8f526a0f
Diego <a00827847@itesm.mx>
2020-10-24 14:07:13 -0500

    Final steps

--- Act-3.4/Act-3.4/Registro.h
+++ Act-3.4/Act-3.4/Registro.h
@@ -11,19 +11,27 @@ struct Registro {
     string ip;
     int cant;
     Registro(string ip, int cant);
-    Registro() {
-        
-    }
+    Registro();
+    void operator=(const Registro &R);
     bool operator>(Registro r2);
     bool operator>=(Registro r2);
     bool operator<(Registro r2);
     bool operator<=(Registro r2);
     bool operator==(Registro r2);
 };
+void Registro::operator=(const Registro &R) {
+    ip = R.ip;
+    cant = R.cant;
+}
 
 Registro::Registro(string ip, int cant) {
     this-> ip = ip;
     this-> cant = cant;
+}
+
+Registro::Registro() {
+    ip = "/";
+    cant = 0;
 }
 
 bool Registro:: operator>(Registro r2) {
--- Act-3.4/Act-3.4/main.cpp
+++ Act-3.4/Act-3.4/main.cpp
@@ -18,14 +18,14 @@ using namespace std;
  cd Documents/TEC/"SEMESTRE 3"/"Estructura de datos"/MyEvidences
  */
 
-
-
+// Complejidad O(n)
 void leerArchivo(DoublyLinkedList<string> &list) {
+    /* Lee archivo de entrada */
     string mes, hora, ipAddress, fallo;
     int dia;
     ifstream archivoEntrada;
     
-    archivoEntrada.open("BitacoraTest.txt");
+    archivoEntrada.open("Bitacora.txt");
     while(archivoEntrada >> mes >> dia >> hora >> ipAddress) { // Lee toda la información
         getline(archivoEntrada, fallo); // Leer el mensaje de error de login
         fallo.erase(fallo.begin());
@@ -35,7 +35,9 @@ void leerArchivo(DoublyLinkedList<string> &list) {
     archivoEntrada.close();
 }
 
+// Complejidad O(n^2)
 void createObject(DoublyLinkedList<string> list, DoublyLinkedList<Registro> &listaRegistros){
+    /* Utiliza la lista ordenada de IPs y crea objetos donde se almacena nombre y cantidad de ocurrencias */
     Registro miRegistro;
     int size = list.getSize();
     int cont = 1;
@@ -43,13 +45,14 @@ void createObject(DoublyLinkedList<string> list, DoublyLinkedList<Registro> &lis
     string ipActual;
     
     while (cont < size) {
-        while(list[cont] == list[cont + 1]) {
+        while(list[cont] == list[cont + 1] && cont < size - 1) {
+            /* Cuenta la cantidad de veces que se repite un IP */
             cantVeces++;
             cont++;
         }
         ipActual = list[cont];
         miRegistro = Registro(ipActual, cantVeces);
-        listaRegistros.addLast(miRegistro);
+        listaRegistros.addLast(miRegistro); // Se almacena objeto de tipo Registro
         cantVeces = 1;
         cont++;
     }
@@ -57,21 +60,42 @@ void createObject(DoublyLinkedList<string> list, DoublyLinkedList<Registro> &lis
     
 }
 
+// Complejidad O(n)
+void displayRepeated(Heap<Registro> ipHeap) {
+    /* Muestra cinco registros más repetidos */
+    cout << " IP Address " << "\tRepeticiones" << endl;
+    for (int i = 0; i < 5; i++) {
+        Registro mayor = ipHeap.remove();
+        cout << mayor.ip << "\t  \t" << mayor.cant << endl;
+    }
+}
+
+// Complejidad O(n)
+void escribeArchivo(DoublyLinkedList<Registro> myList) {
+    /* Genera archvi con objetos de tipo Registro */
+    ofstream archivoFinal;
+    archivoFinal.open("Salida.txt");
+    HeapSort(myList, "Descending");
+    for (int i = 1; i < myList.getSize(); i++) {
+        Registro salida = myList[i];
+        archivoFinal << salida.ip << "\t  \t" << salida.cant << endl;
+    }
+    
+    archivoFinal.close();
+}
+
 int main() {
+    Heap<Registro> ipHeap;
+    DoublyLinkedList<string> list; // Almacena IPs de archivo
+    DoublyLinkedList<Registro> myList; // Almacena Registros de list
     
-    Registro uno;
-    uno = Registro("123", 4);
-    Registro dos;
-    dos = Registro("123", 6);
-    
-    bool oneIsGreater = uno > dos;
-    cout << oneIsGreater << endl;
-    DoublyLinkedList<string> list;
-    DoublyLinkedList<Registro> myList;
-    Heap<string> ipHeap;
     leerArchivo(list);
-    HeapSort(list);
+    HeapSort(list); // los ordena por tipo de ip, los que son iguales quedan juntos
     createObject(list, myList);
+    ipHeap = myList;
+    
+    escribeArchivo(myList);
+    displayRepeated(ipHeap);
     
     return 0;
 }
