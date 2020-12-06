73691c245463023efb590277abbc276668e14e50
Diego <A00827847@itesm.mx>
2020-12-06 00:06:29 -0600

    Observaciones

---
+++ Act-4.3/Act-4.3/Heap.h
@@ -0,0 +1,12 @@
+//
+//  Heap.h
+//  Act-4.3
+//
+//  Created by Diego Solis on 12/5/20.
+//
+
+#ifndef Heap_h
+#define Heap_h
+
+
+#endif /* Heap_h */
---
+++ Act-4.3/Act-4.3/Registro.h
@@ -0,0 +1,12 @@
+//
+//  Registro.h
+//  Act-4.3
+//
+//  Created by Diego Solis on 12/5/20.
+//
+
+#ifndef Registro_h
+#define Registro_h
+
+
+#endif /* Registro_h */
